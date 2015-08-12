1457a04c4b184a042bd4d1af37754b4014715de2
lasershow <genn777f3@gmail.com>
2015-08-13 02:07:29 +0900

    set root URL top index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'top/index'
+  root 'top#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
